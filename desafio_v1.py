2346b5f0dda9481a64558806ecb8ee3960ea4016
André Luiz Lessa <49814751+andrelessssa@users.noreply.github.com>
2024-05-10 19:52:00 -0300

    :wrench:	chore continuando...
    :wrench:	chore continuando...

--- desafio_v1.py
+++ desafio_v1.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractclassmethod,abstractproperty
+import textwrap
 from _datetime import datetime
 
 class Cliente:
@@ -121,30 +122,81 @@ class Historico:
     def __init__(self):
         self._transacoes = []
 
-@property
-def transacoes(self):
-    return self._transacoes
+    @property
+    def transacoes(self):
+        return self._transacoes
 
-def adicionar_transacao(self, transacao):
-    self._transacoes.append(
+    def adicionar_transacoes(self, transacao):
+        self._transacoes.append(
         {
             "tipo": transacao.__class__.__name__,
             "valor": transacao.valor,
             "data": datetime.now().strftime("%d-%m-%y %H:%M:%S"),
         }
-    )
-
+    )   
+    
 
 class Transacao(ABC):
-    pass
+    @property
+    @abstractproperty
+    def valor(self):
+        pass
+    @abstractclassmethod
+    def registrar(self, conta):
+        pass
 
 
 class Saque(Transacao):
-    pass
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+
+    def registrar(self,conta):
+        sucesso_transacao = conta.sacar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)    
 
 
 class Deposito(Transacao):
-    pass
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+
+    def registrar(self,conta):
+        sucesso_transacao = conta.depositar(self.valor)
+
+        if sucesso_transacao:
+            conta.historico.adicionar_transacao(self)    
 
 
 
+
+def menu():
+
+    menu = """\n
+    ================= MENU ==================
+    [d]\tDepositar
+    [s]\tSacar
+    [e]\tExtrato
+    [nc]\tNova Conta
+    [lc]\tListar contas
+    [nu]\tNovo usuário
+    [q]\tSair
+
+"""
+    return input(textwrap.dedent(menu))
+
+def filtrar_cliente(cpf, clientes):
+    clientes_filtrados = [cliente for cliente in clientes if cliente.cpf == cpf]
+    return clientes_filtrados[0] if clientes_filtrados else None
+
+
+def recuperar_conta_cliente(cliente):
+    if
